26c693de6193e76600b54c7380be6d950a860aa8
Nihar Kanta Tripathy <niharkanta1@live.com>
2023-03-02 16:22:48 +0530

    Falling Platform Finishing and Spike Ball

--- Game/Platforms/FallingPlatform.cs
+++ Game/Platforms/FallingPlatform.cs
@@ -8,8 +8,10 @@ public class FallingPlatform : Node2D
 {
     private Tween _tween;
     private AnimatedSprite _sprite;
-    private Timer _timer;
-
+    private Timer _timer, _fallTimer;
+    private Particles2D _fallParticle, _dustParticle;
+    private CollisionShape2D _collisionShape2D, _activationCollisionArea;
+        
     [Export] private float _xMax = 2.0f;
     [Export] private float _rMax = 5f;
 
@@ -21,7 +23,8 @@ public class FallingPlatform : Node2D
     private float _x, _r;
     private int _currentDir = 0;
     private bool _left = true;
-    private bool _tweenMe;
+    private bool _isShaking;
+    private bool _isFalling, _isActivated;
 
     //[Signal] public delegate void TweenFinished(int value);
 	
@@ -29,35 +32,36 @@ public class FallingPlatform : Node2D
     {
         _tween = GetNode<Tween>("ShakeTween");
         _sprite = GetNode<AnimatedSprite>("AnimatedSprite");
-        _timer = GetNode<Timer>("Timer");
+        _timer = GetNode<Timer>("Timers/Timer");
+        _fallTimer = GetNode<Timer>("Timers/FallTimer");
+        _fallParticle = GetNode<Particles2D>("Particles/FallParticle");
+        _dustParticle = GetNode<Particles2D>("Particles/DustParticle");
+        _collisionShape2D = GetNode<CollisionShape2D>("StaticBody2D/CollisionShape2D");
+        _activationCollisionArea = GetNode<CollisionShape2D>("Area2D/CollisionShape2D");
         _timer.WaitTime = _tweenDuration;
         _currentDir = -1;
     }
 
     public override void _Process(float delta)
     {
-        if (!_tweenMe) return;
+        if (!_isShaking) return;
         if (_x > _stopThreshold)
         {
-            switch (_currentDir)
-            {
-                case -1:
-                    TiltLeft(_x, _r);
-                    break;
-                case 1:
-                    TiltRight(_x, _r);
-                    break;
-            }
-        } 
+            TweenObject(_x, _r, _currentDir);
+        }
         else
         {
-            _tweenMe = false;
+            _isShaking = false;
+        }
+        if (_isFalling && !_isShaking)
+        {
+            Fall();
         }
     }
 
     private void StartTween()
     {
-        _tweenMe = true;
+        _isShaking = true;
         _x = _xMax;
         _r = _rMax;
     }
@@ -92,7 +96,7 @@ public class FallingPlatform : Node2D
         );
         
         _tween.Start();
-        if (!_tweenMe)
+        if (!_isShaking)
         {
             _timer.Stop();
         }
@@ -103,69 +107,64 @@ public class FallingPlatform : Node2D
             _currentDir = _left ? -1 : 1;
         }
     }
-    
-    private void TiltLeft(float x, float r)
-    {
-        _tween.InterpolateProperty(
-            _sprite,
-            "position:x",
-            0,
-            -x,
-            _tweenDuration,
-            _transitionType,
-            Tween.EaseType.Out
-            );
-        
-        _tween.InterpolateProperty(
-            _sprite,
-            "rotation_degrees",
-            0,
-            r,
-            _tweenDuration,
-            _transitionType,
-            Tween.EaseType.Out
-        );
-        _tween.Start();
-        _timer.Start();
-        _currentDir = 0;
-    }
-    
-    private void TiltRight(float x, float r)
-    {
-        _tween.InterpolateProperty(
-            _sprite,
-            "position:x",
-            0,
-            x,
-            _tweenDuration,
-            _transitionType,
-            Tween.EaseType.Out
-        );
-        
-        _tween.InterpolateProperty(
-            _sprite,
-            "rotation_degrees",
-            0,
-            r,
-            _tweenDuration,
-            _transitionType,
-            Tween.EaseType.Out
-        );
-        _timer.Start();
-        _tween.Start();
-        _currentDir = 0;
-    }
 
+
+    private void TweenObject(float x, float r, int dir)
+    {
+        if (dir == 0) return;
+        _tween.InterpolateProperty(
+            _sprite,
+            "position:x",
+            0,
+            x * dir,
+            _tweenDuration,
+            _transitionType,
+            Tween.EaseType.Out
+        );
+        
+        _tween.InterpolateProperty(
+            _sprite,
+            "rotation_degrees",
+            0,
+            r,
+            _tweenDuration,
+            _transitionType,
+            Tween.EaseType.Out
+        );
+        _tween.Start();
+        _timer.Start();
+        _currentDir = 0;
+    }
+    
+    private void Fall()
+    {
+        _fallParticle.Emitting = true;
+        _isFalling = false; // Only Once we need to call Fall Method.
+        _collisionShape2D.Disabled = true;
+        _activationCollisionArea.Disabled = true;
+        _sprite.Visible = false;
+    }
+   
     public void OnArea2DBodyShapeEntered(RID bodyRid, Node body, int bodyShapeIndex, int localShapeIndex)
     {
-        if (!(body is PlayerTwo))
+        if (!(body is PlayerTwo) && _isActivated)
             return;
         StartTween();
-
+        _fallTimer.Start();
+        _isActivated = true;
     }
     
     private void OnTimerTimeout()
     {
         TweenFinished();
     }
+
+    private void OnFallTimerTimeout()
+    {
+        StartTween();
+        _sprite.Play("Off");
+        _dustParticle.Emitting = false;
+        _isFalling = true;
+    }
+    
 }
